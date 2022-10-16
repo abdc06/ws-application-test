74cfabd1a6c4f86ce32641fd226c02e5f7b87b38
abdc06 <abdc2806@gmail.com>
2022-10-16 10:28:38 +0900

    junit5-getting-started 프로젝트 생성

---
+++ junit5-getting-started/src/main/java/me/abdc/junit5gettingstarted/Junit5GettingStartedApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.junit5gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5GettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5GettingStartedApplication.class, args);
+    }
+
+}
---
+++ junit5-getting-started/src/test/java/me/abdc/junit5gettingstarted/Junit5GettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.junit5gettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5GettingStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
