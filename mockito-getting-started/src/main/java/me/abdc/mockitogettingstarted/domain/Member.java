1f04a8e32ec8a131f73159bf08a961d394d932e6
abdc06 <abdc2806@gmail.com>
2022-10-16 20:15:51 +0900

    샘플 도메인 (멤버, 스터디) 추가

---
+++ mockito-getting-started/src/main/java/me/abdc/mockitogettingstarted/domain/Member.java
@@ -0,0 +1,10 @@
+package me.abdc.mockitogettingstarted.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+
+    private Long memberId;
+}
---
+++ mockito-getting-started/src/main/java/me/abdc/mockitogettingstarted/domain/Study.java
@@ -0,0 +1,10 @@
+package me.abdc.mockitogettingstarted.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Study {
+
+    private Member owner;
+}
