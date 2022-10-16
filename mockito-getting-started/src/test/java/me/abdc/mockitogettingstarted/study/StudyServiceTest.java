a3a0b71084b3450b30cf7be8d685261e2d734634
abdc06 <abdc2806@gmail.com>
2022-10-16 20:21:04 +0900

    Mock 필드 네임 수정

--- mockito-getting-started/src/test/java/me/abdc/mockitogettingstarted/study/StudyServiceTest.java
+++ mockito-getting-started/src/test/java/me/abdc/mockitogettingstarted/study/StudyServiceTest.java
@@ -1,5 +1,6 @@
 package me.abdc.mockitogettingstarted.study;
 
+import me.abdc.mockitogettingstarted.domain.Member;
 import me.abdc.mockitogettingstarted.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,27 +8,29 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
     @Mock
-    MemberService memberService1;
+    MemberService memberService;
 
     @Mock
-    StudyRepository studyRepository1;
+    StudyRepository studyRepository;
 
     @Test
     void createStudyService(@Mock MemberService memberService2, @Mock StudyRepository studyRepository2) {
         MemberService memberService3 = Mockito.mock(MemberService.class);
         StudyRepository studyRepository3 = Mockito.mock(StudyRepository.class);
 
-        StudyService studyService1 = new StudyService(memberService1, studyRepository1);
+        StudyService studyService = new StudyService(memberService, studyRepository);
         StudyService studyService2 = new StudyService(memberService2, studyRepository2);
         StudyService studyService3 = new StudyService(memberService3, studyRepository3);
 
-        assertNotNull(studyService1);
+        assertNotNull(studyService);
         assertNotNull(studyService2);
         assertNotNull(studyService3);
     }
