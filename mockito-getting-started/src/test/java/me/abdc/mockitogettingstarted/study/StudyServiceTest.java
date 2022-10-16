ce64c9742dd381761ebce8f9011fcdfa92426fe8
abdc06 <abdc2806@gmail.com>
2022-10-17 05:19:02 +0900

    Mock 객체 확인, BDD 스타일 테스트

--- mockito-getting-started/src/test/java/me/abdc/mockitogettingstarted/study/StudyServiceTest.java
+++ mockito-getting-started/src/test/java/me/abdc/mockitogettingstarted/study/StudyServiceTest.java
@@ -13,6 +13,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -86,5 +88,47 @@ class StudyServiceTest {
 
         assertNotNull(study.getOwner());
         assertEquals(member, study.getOwner());
+    }
+
+    @Test
+    void verification() {
+        verifyNoInteractions(memberService);
+
+        memberService.validate(1L);
+        memberService.validate(1L);
+        memberService.validate(2L);
+
+        verify(memberService, times(2)).validate(1L);
+        verify(memberService, times(1)).validate(2L);
+        verify(memberService, never()).validate(3L);
+
+        verifyNoMoreInteractions(memberService);
+    }
+
+    @Test
+    void bddStyle() {
+        // Given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("abdc2806@email.com");
+
+        Study mockStudy = new Study();
+        mockStudy.setLimitCount(10);
+        mockStudy.setName("테스트");
+
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(mockStudy)).willReturn(mockStudy);
+
+        // When
+        Study study = studyService.createNewStudy(1L, mockStudy);
+        memberService.validate(1L);
+
+        // Then
+        assertNotNull(study.getOwner());
+        assertEquals(member, study.getOwner());
+        then(memberService).should(times(1)).validate(1L);
+        then(memberService).shouldHaveNoMoreInteractions();
     }
 }
