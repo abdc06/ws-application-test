869a9e9d57f8d38ffd8044a872e102c985723be7
abdc06 <abdc2806@gmail.com>
2022-10-17 12:20:55 +0900

    testcontainers-getting-started 프로젝트 생성

---
+++ testcontainers-getting-started/src/main/java/me/abdc/testcontainersgettingstarted/TestcontainersGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.testcontainersgettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersGettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcontainersGettingStartedApplication.class, args);
+    }
+
+}
---
+++ testcontainers-getting-started/src/test/java/me/abdc/testcontainersgettingstarted/TestcontainersGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.testcontainersgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
