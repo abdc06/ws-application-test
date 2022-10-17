e43456c3e54c208020baa9c051ca25c5b8bb0db5
abdc06 <abdc2806@gmail.com>
2022-10-17 21:42:28 +0900

    jmeter-getting-started 프로젝트 생성

---
+++ jmeter-getting-started/src/main/java/me/abdc/jmetergettingstarted/JmeterGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.jmetergettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterGettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmeterGettingStartedApplication.class, args);
+    }
+
+}
